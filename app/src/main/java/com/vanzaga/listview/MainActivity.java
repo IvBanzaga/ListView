d84406ecd705acb0f3d4fb30578683292bc35f2d
Ivan Bazaga <ivan.cpweb@gmail.com>
2025-02-12 11:41:33 +0000

    Proyecto finalisado

--- app/src/main/java/com/vanzaga/listview/MainActivity.java
+++ app/src/main/java/com/vanzaga/listview/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Variables
     private TextView tv1;
     private ListView lv1;
 
@@ -24,16 +25,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
+        // Asignar variables a los elementos del layout del diseño de la interfaz
         tv1 = findViewById(R.id.textView);
         lv1 = findViewById(R.id.ListView);
 
+        // Creamos un array de nombres y edades
         String nombres [] = {"Juan", "Pedro", "Luis", "Carlos", "Ana", "Maria", "Laura", "Sofia", "Lucia", "Marta"};
         String edades [] = {"20", "25", "30", "35", "40", "45", "50", "55", "60", "65"};
 
+        // Creamos un adaptador
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_escoger, nombres);
-        /*lv1.setAdapter(adapter);*/
+        lv1.setAdapter(adapter);
 
+        // Creamos el evento para el ListView
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
